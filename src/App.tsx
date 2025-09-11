8e8723d842a1670993b26bc4ab66af7825fd3e48
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-11 15:37:22 -0300

    Create main title

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles.styled";
+
+const MainTitleStyled = styled.h1`
+  --spacing: 10px;
+  color: var(--color-green-900);
+  font-size: 25px;
+  text-transform: uppercase;
+  text-align: center;
+  margin-top: 40px;
+  letter-spacing: var(--spacing);
+  padding-left: var(--spacing);
+`;
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
-      <div>Hello world</div>
+      <MainTitleStyled>
+        Spli
+        <br />
+        tter
+      </MainTitleStyled>
     </>
   );
 };
