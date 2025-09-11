baed0dc34ef84e32575bc5ab3499fd99207f3fac
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-11 20:46:08 -0300

    Create styled button

---
+++ src/styles/Button.styled.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const TipButtonStyled = styled.button`
+  background-color: var(--color-green-900);
+  border: 2px solid transparent;
+  border-radius: 0.3125remx;
+  color: var(--color-white);
+  font-size: 1.5rem;
+  padding: 0.3125rem 0;
+  text-align: center;
+`;
