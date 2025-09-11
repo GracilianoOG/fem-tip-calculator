ee24e43b3a76d181437920b476af3873341ef2eb
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-11 20:53:50 -0300

    Replace static icon with a dynamic URL

--- src/components/LabeledInput/styles.ts
+++ src/components/LabeledInput/styles.ts
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import DollarIcon from "../../assets/images/icon-dollar.svg";
 
 export const LabelStyled = styled.label`
   color: var(--color-grey-500);
@@ -7,11 +6,11 @@ export const LabelStyled = styled.label`
   margin-bottom: 0.375rem;
 `;
 
-export const InputStyled = styled.input`
+export const InputStyled = styled.input<{ $icon: string }>`
   border: none;
   border-radius: 5px;
   background-color: var(--color-grey-50);
-  background-image: url("${DollarIcon}");
+  background-image: url("${({ $icon }) => $icon}");
   background-repeat: no-repeat;
   background-position: center left 1.125rem;
   color: var(--color-green-900);
