25de7582d3a6f6a60540cd31e3a408ebb3afd172
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-11 20:47:39 -0300

    Fix misspelled unit

--- src/styles/Button.styled.ts
+++ src/styles/Button.styled.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const TipButtonStyled = styled.button`
   background-color: var(--color-green-900);
   border: 2px solid transparent;
-  border-radius: 0.3125remx;
+  border-radius: 0.3125rem;
   color: var(--color-white);
   font-size: 1.5rem;
   padding: 0.3125rem 0;
