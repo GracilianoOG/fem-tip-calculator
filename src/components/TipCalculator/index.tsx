d098237b12a748115e7a11359d4dfe60e9147e1f
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-11 22:58:53 -0300

    Create TipCalculator component

---
+++ src/components/TipCalculator/index.tsx
@@ -0,0 +1,37 @@
+import LabeledInput from "../LabeledInput";
+import ResultPanel from "../ResultPanel";
+import TipMenu from "../TipMenu";
+import dollarIcon from "./assets/images/icon-dollar.svg";
+import personIcon from "./assets/images/icon-person.svg";
+import { AppWrapperStyled, InputsWrapper } from "./styles";
+
+const TipCalculator = () => {
+  return (
+    <AppWrapperStyled>
+      <InputsWrapper>
+        <div>
+          <LabeledInput
+            id="price"
+            label="Bill"
+            placeholder="0"
+            iconSrc={dollarIcon}
+          />
+        </div>
+        <div>
+          <TipMenu label="Select Tip %" />
+        </div>
+        <div>
+          <LabeledInput
+            id="people"
+            label="Number of People"
+            placeholder="0"
+            iconSrc={personIcon}
+          />
+        </div>
+      </InputsWrapper>
+      <ResultPanel />
+    </AppWrapperStyled>
+  );
+};
+
+export default TipCalculator;
---
+++ src/components/TipCalculator/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const AppWrapperStyled = styled.div`
+  background-color: var(--color-white);
+  border-radius: 1.5625rem;
+  display: flex;
+  flex-direction: column;
+  gap: 2.5rem;
+  padding: 2rem;
+`;
+
+export const InputsWrapper = styled.div`
+  display: flex;
+  gap: 2rem;
+  flex-direction: column;
+`;
