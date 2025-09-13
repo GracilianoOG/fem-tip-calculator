f6c113f5acd7b44f140ab6d887d06e7637e60e47
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-13 10:26:38 -0300

    Break grid into 3 columns on laptop

--- src/components/TipMenu/styles.ts
+++ src/components/TipMenu/styles.ts
@@ -1,7 +1,12 @@
 import styled from "styled-components";
+import { mediaQueries } from "../../utils/mediaQueries";
 
 export const TipOptionsStyled = styled.ul`
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 14px 16px;
+
+  ${mediaQueries.laptop} {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
 `;
