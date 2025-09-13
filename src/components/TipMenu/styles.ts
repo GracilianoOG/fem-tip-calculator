8c95048cb0320f07704f979303a6adb4b66349d0
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-13 19:44:45 -0300

    Convert px units to rem

--- src/components/TipMenu/styles.ts
+++ src/components/TipMenu/styles.ts
@@ -4,7 +4,7 @@ import { mediaQueries } from "../../utils/mediaQueries";
 export const TipOptionsStyled = styled.ul`
   display: grid;
   grid-template-columns: 1fr 1fr;
-  gap: 14px 16px;
+  gap: 0.875rem 1rem;
 
   ${mediaQueries.tablet} {
     grid-template-columns: 1fr 1fr 1fr;
