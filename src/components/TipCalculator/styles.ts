91e77c2309e4fa5ef292fc63e9bebafec2cb086f
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-13 10:45:53 -0300

    Style layout to a vertical design for wide screen

--- src/components/TipCalculator/styles.ts
+++ src/components/TipCalculator/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { mediaQueries } from "../../utils/mediaQueries";
 
 export const AppWrapperStyled = styled.div`
   background-color: var(--color-white);
@@ -7,10 +8,25 @@ export const AppWrapperStyled = styled.div`
   flex-direction: column;
   gap: 2.5rem;
   padding: 2rem;
+
+  ${mediaQueries.laptop} {
+    flex-direction: row;
+    height: 30rem;
+    justify-content: space-between;
+    margin: auto;
+    padding: 2rem;
+    width: 57.5rem;
+  }
 `;
 
 export const InputsWrapper = styled.div`
   display: flex;
   gap: 2rem;
   flex-direction: column;
+
+  ${mediaQueries.laptop} {
+    flex: 1;
+    justify-content: space-between;
+    padding: 1rem;
+  }
 `;
