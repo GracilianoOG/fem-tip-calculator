aad60f4761995b90e69664148d2693a34d4ec385
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-13 12:18:41 -0300

    Create InputWithIconStyled
    Removed local InputStyled. InputWithIconStyled inherits from InputStyled.

--- src/components/LabeledInput/styles.ts
+++ src/components/LabeledInput/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { InputStyled } from "../../styles/Input.styled";
 
 export const LabelStyled = styled.label`
   color: var(--color-grey-500);
@@ -6,18 +7,9 @@ export const LabelStyled = styled.label`
   margin-bottom: 0.375rem;
 `;
 
-export const InputStyled = styled.input<{ $icon: string }>`
-  border: none;
-  border-radius: 5px;
-  background-color: var(--color-grey-50);
+export const InputWithIconStyled = styled(InputStyled)<{ $icon: string }>`
   background-image: url("${({ $icon }) => $icon}");
   background-repeat: no-repeat;
   background-position: center left 1.125rem;
-  color: var(--color-green-900);
-  display: block;
-  font-size: 1.5rem;
-  min-height: 2.9375rem;
   padding: 0 1rem 0 2.8125rem;
-  text-align: right;
-  width: 100%;
 `;
