b574ae2d192a35f46d7a570e5e06d80b19bf818d
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-13 19:42:34 -0300

    Make TipCalculator more responsive
    - Center container vertically by default.
    - Get max container size from --max-width variable.

--- src/components/TipCalculator/styles.ts
+++ src/components/TipCalculator/styles.ts
@@ -2,19 +2,21 @@ import styled from "styled-components";
 import { mediaQueries } from "../../utils/mediaQueries";
 
 export const AppWrapperStyled = styled.div`
+  --max-width: 37.5rem;
   background-color: var(--color-white);
   border-radius: 1.5625rem;
   display: grid;
   gap: 2.5rem;
+  margin: 0 auto;
   padding: 2rem;
+  width: min(100%, var(--max-width));
 
   ${mediaQueries.laptop} {
+    --max-width: 57.5rem;
     grid-template-columns: 1fr 1fr;
     gap: 1.875rem;
     height: 30rem;
-    margin: auto;
     padding: 2rem;
-    width: 57.5rem;
   }
 `;
 
