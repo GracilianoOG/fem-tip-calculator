be2966f42701f10c3ef7535d7b9c599d0bf130d2
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-13 12:18:30 -0300

    Create global styled input

---
+++ src/styles/Input.styled.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const InputStyled = styled.input`
+  border: none;
+  border-radius: 0.3125rem;
+  background-color: var(--color-grey-50);
+  color: var(--color-green-900);
+  display: block;
+  font-size: 1.5rem;
+  min-height: 2.9375rem;
+  padding-right: 1rem;
+  text-align: right;
+  width: 100%;
+`;
