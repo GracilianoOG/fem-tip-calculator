6905cb124a48e6a8c7e4a440e2651596fbbe49cf
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-14 16:48:26 -0300

    Add setTip to each button

--- src/components/TipMenu/index.tsx
+++ src/components/TipMenu/index.tsx
@@ -1,18 +1,21 @@
+import useTipValues from "../../hooks/useTipValues";
 import { TipButtonStyled } from "../../styles/Button.styled";
 import { InputStyled } from "../../styles/Input.styled";
 import { TipLabelStyled } from "../../styles/Label.styled";
 import { TipOptionsStyled } from "./styles";
 
 const TipMenu = ({ label }: { label: string }) => {
+  const { setTip } = useTipValues();
+
   return (
     <>
       <TipLabelStyled htmlFor="custom-tip">{label}</TipLabelStyled>
       <TipOptionsStyled>
-        <TipButtonStyled>5%</TipButtonStyled>
-        <TipButtonStyled>10%</TipButtonStyled>
-        <TipButtonStyled>15%</TipButtonStyled>
-        <TipButtonStyled>25%</TipButtonStyled>
-        <TipButtonStyled>50%</TipButtonStyled>
+        <TipButtonStyled onClick={() => setTip(0.05)}>5%</TipButtonStyled>
+        <TipButtonStyled onClick={() => setTip(0.1)}>10%</TipButtonStyled>
+        <TipButtonStyled onClick={() => setTip(0.15)}>15%</TipButtonStyled>
+        <TipButtonStyled onClick={() => setTip(0.25)}>25%</TipButtonStyled>
+        <TipButtonStyled onClick={() => setTip(0.5)}>50%</TipButtonStyled>
         <InputStyled id="custom-tip" placeholder="Custom" />
       </TipOptionsStyled>
     </>
