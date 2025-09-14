30ccac4ad3f4e6670d26260b3109b6164b635d2e
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-14 16:18:29 -0300

    Wrap TipCalculator elements with TipProvider

--- src/components/TipCalculator/index.tsx
+++ src/components/TipCalculator/index.tsx
@@ -4,33 +4,36 @@ import TipMenu from "../TipMenu";
 import dollarIcon from "../../assets/images/icon-dollar.svg";
 import personIcon from "../../assets/images/icon-person.svg";
 import { AppWrapperStyled, InputsWrapper } from "./styles";
+import TipProvider from "../../providers/TipProvider";
 
 const TipCalculator = () => {
   return (
-    <AppWrapperStyled>
-      <InputsWrapper>
-        <div>
-          <LabeledInput
-            id="price"
-            label="Bill"
-            placeholder="0"
-            iconSrc={dollarIcon}
-          />
-        </div>
-        <div>
-          <TipMenu label="Select Tip %" />
-        </div>
-        <div>
-          <LabeledInput
-            id="people"
-            label="Number of People"
-            placeholder="0"
-            iconSrc={personIcon}
-          />
-        </div>
-      </InputsWrapper>
-      <ResultPanel />
-    </AppWrapperStyled>
+    <TipProvider>
+      <AppWrapperStyled>
+        <InputsWrapper>
+          <div>
+            <LabeledInput
+              id="price"
+              label="Bill"
+              placeholder="0"
+              iconSrc={dollarIcon}
+            />
+          </div>
+          <div>
+            <TipMenu label="Select Tip %" />
+          </div>
+          <div>
+            <LabeledInput
+              id="people"
+              label="Number of People"
+              placeholder="0"
+              iconSrc={personIcon}
+            />
+          </div>
+        </InputsWrapper>
+        <ResultPanel />
+      </AppWrapperStyled>
+    </TipProvider>
   );
 };
 
