2052695a469333dbc88b8bc89835d268cfaedd3a
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-14 16:49:17 -0300

    Calculate and pass tip amount and total

--- src/components/ResultPanel/index.tsx
+++ src/components/ResultPanel/index.tsx
@@ -1,12 +1,20 @@
+import useTipValues from "../../hooks/useTipValues";
 import { ResetButtonStyled } from "../../styles/Button.styled";
 import ResultBox from "../ResultBox";
 import { ResultsWrapperStyled } from "./styles";
 
 const ResultPanel = () => {
+  // Todo: Get context values and calculate tip and total
+  const { bill, tip, people } = useTipValues();
+
+  const billPerPerson = bill / people;
+  const tipAmount = billPerPerson * tip;
+  const totalPerPerson = billPerPerson + tipAmount;
+
   return (
     <ResultsWrapperStyled>
-      <ResultBox label="Tip Amount" total={4.27} />
-      <ResultBox label="Total" total={32.79} />
+      <ResultBox label="Tip Amount" total={tipAmount.toFixed(2)} />
+      <ResultBox label="Total" total={totalPerPerson.toFixed(2)} />
       <ResetButtonStyled>Reset</ResetButtonStyled>
     </ResultsWrapperStyled>
   );
