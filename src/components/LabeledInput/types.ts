0ea8abf2a93a55df1864ebf9cae1a187d260923d
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-17 10:01:54 -0300

    Add validations type

--- src/components/LabeledInput/types.ts
+++ src/components/LabeledInput/types.ts
@@ -1,7 +1,10 @@
+import type { ValidationType } from "../../utils/formValidations";
+
 export interface LabeledInputProps {
   id: string;
   label: string;
   placeholder: string;
   iconSrc: string;
   setValue: React.Dispatch<React.SetStateAction<number>>;
+  validations: ValidationType[];
 }
