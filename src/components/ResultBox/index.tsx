0b4b9be52a9b5c65eb9fbd21908d849dde925540
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-17 11:24:32 -0300

    Format number to USD currency

--- src/components/ResultBox/index.tsx
+++ src/components/ResultBox/index.tsx
@@ -7,12 +7,18 @@ import {
 import type { ResultBoxProps } from "./types";
 
 const ResultBox = ({ label, total }: ResultBoxProps) => {
+  const formatter = new Intl.NumberFormat("en-US", {
+    currency: "USD",
+    style: "currency",
+    currencyDisplay: "symbol",
+  });
+
   return (
     <ResultBoxStyled>
       <ResultTitleStyled>
         {label} <ResultPerPersonStyled>/ person</ResultPerPersonStyled>
       </ResultTitleStyled>
-      <ResultPriceStyled>${total}</ResultPriceStyled>
+      <ResultPriceStyled>{formatter.format(total)}</ResultPriceStyled>
     </ResultBoxStyled>
   );
 };
