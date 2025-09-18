0ffd878a6e5aa0caa4484caf46b11fe2af67f0f4
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-18 11:34:20 -0300

    Validate tip input

--- src/components/TipInput/index.tsx
+++ src/components/TipInput/index.tsx
@@ -1,15 +1,35 @@
 import useInputData from "../../hooks/useInputData";
 import useTipValues from "../../hooks/useTipValues";
 import { InputStyled } from "../../styles/Input.styled";
+import {
+  integerValidation,
+  validateInputElement,
+} from "../../utils/formValidations";
 import type { TipInputProps } from "./types";
 
 const TipInput = ({ id, placeholder }: TipInputProps) => {
   const { setTip } = useTipValues();
-  const { inputData, setInputData } = useInputData();
+  const { inputData, setInputData, setInputErrors } = useInputData();
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTip(parseFloat(e.target.value) * 0.01);
-    setInputData({ ...inputData, tip: e.target.value });
+    const input = e.target as HTMLInputElement;
+    setInputData({ ...inputData, tip: input.value });
+
+    for (const validation of integerValidation) {
+      const [isInvalid, message] = validateInputElement(validation, input);
+
+      if (isInvalid) {
+        setInputErrors(prevErrors => ({
+          ...prevErrors,
+          tip: message as string,
+        }));
+        setTip(0);
+        return;
+      }
+    }
+
+    setInputErrors(prevErrors => ({ ...prevErrors, tip: "" }));
+    setTip(parseFloat(input.value) * 0.01);
   };
 
   return (
