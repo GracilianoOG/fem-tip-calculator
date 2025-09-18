d9ae30d4337519f381b57e1463c82c53788d3aa3
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-18 11:32:32 -0300

    Clear tip error on click

--- src/components/TipButton/index.tsx
+++ src/components/TipButton/index.tsx
@@ -1,13 +1,17 @@
+import useInputData from "../../hooks/useInputData";
 import useTipValues from "../../hooks/useTipValues";
 import { TipButtonStyled } from "./styles";
 import type { TipButtonProps } from "./types";
 
 const TipButton = ({ label, tipPercent }: TipButtonProps) => {
   const { tip, setTip } = useTipValues();
+  const { setInputErrors } = useInputData();
+
   const tipDecimal = tipPercent * 0.01;
 
   const handleClick = () => {
     setTip(tipDecimal);
+    setInputErrors(prevErrors => ({ ...prevErrors, tip: "" }));
   };
 
   return (
