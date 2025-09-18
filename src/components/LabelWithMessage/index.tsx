4bb23f37e401ce88f30bd9992e79eab7177fd610
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-18 09:28:44 -0300

    Use message as text content of span

--- src/components/LabelWithMessage/index.tsx
+++ src/components/LabelWithMessage/index.tsx
@@ -2,15 +2,11 @@ import { LabelStyled } from "../../styles/Label.styled";
 import { LabelMessageStyled, LabelWrapperStyled } from "./styles";
 import type { LabelWithMessageProps } from "./types";
 
-const LabelWithMessage = ({
-  id,
-  label,
-  messageElRef,
-}: LabelWithMessageProps) => {
+const LabelWithMessage = ({ id, label, message }: LabelWithMessageProps) => {
   return (
     <LabelWrapperStyled>
       <LabelStyled htmlFor={id}>{label}</LabelStyled>
-      <LabelMessageStyled ref={messageElRef}></LabelMessageStyled>
+      <LabelMessageStyled>{message}</LabelMessageStyled>
     </LabelWrapperStyled>
   );
 };
