90099b2465ea23e9aa0f9255df1a8b8919d05565
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-18 08:29:03 -0300

    Control TipInput

--- src/components/TipInput/index.tsx
+++ src/components/TipInput/index.tsx
@@ -1,16 +1,24 @@
+import useInputData from "../../hooks/useInputData";
 import useTipValues from "../../hooks/useTipValues";
 import { InputStyled } from "../../styles/Input.styled";
 import type { TipInputProps } from "./types";
 
 const TipInput = ({ id, placeholder }: TipInputProps) => {
   const { setTip } = useTipValues();
+  const { inputData, setInputData } = useInputData();
 
   const handleClick = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTip(parseFloat(e.target.value) * 0.01);
+    setInputData({ ...inputData, tip: e.target.value });
   };
 
   return (
-    <InputStyled id={id} placeholder={placeholder} onChange={handleClick} />
+    <InputStyled
+      id={id}
+      placeholder={placeholder}
+      onChange={handleClick}
+      value={inputData.tip}
+    />
   );
 };
 
