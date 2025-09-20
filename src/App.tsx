af094004170c78f68c635c646a12c1316cf4bd92
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-20 13:33:12 -0300

    Import Header component
    Removed title styled styles and imports.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
-import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles.styled";
 import TipCalculator from "./components/TipCalculator";
-import { mediaQueries } from "./utils/mediaQueries";
 import TipProvider from "./providers/TipProvider";
 import InputDataProvider from "./providers/InputDataProvider";
-
-const MainTitleStyled = styled.h1`
-  --spacing: 10px;
-  color: var(--color-green-900);
-  font-size: 25px;
-  text-transform: uppercase;
-  text-align: center;
-  margin-bottom: 30px;
-  margin-top: 40px;
-  letter-spacing: var(--spacing);
-  padding-left: var(--spacing);
-
-  ${mediaQueries.laptop} {
-    margin-bottom: 78px;
-    margin-top: 152px;
-  }
-`;
+import Header from "./components/Header";
 
 const App = () => {
   return (
@@ -28,11 +10,7 @@ const App = () => {
       <TipProvider>
         <InputDataProvider>
           <GlobalStyles />
-          <MainTitleStyled>
-            Spli
-            <br />
-            tter
-          </MainTitleStyled>
+          <Header />
           <TipCalculator />
         </InputDataProvider>
       </TipProvider>
