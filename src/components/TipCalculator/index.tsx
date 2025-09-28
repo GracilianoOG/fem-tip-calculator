8c5c91564d99a6b26de619c0809ba937ffe15294
Gabriel <gabriel.gracilianomb@gmail.com>
2025-09-28 16:49:49 -0300

    Import InputsPanel

--- src/components/TipCalculator/index.tsx
+++ src/components/TipCalculator/index.tsx
@@ -1,48 +1,11 @@
-import LabeledInput from "../LabeledInput";
 import ResultPanel from "../ResultPanel";
-import TipMenu from "../TipMenu";
-import dollarIcon from "../../assets/images/icon-dollar.svg";
-import personIcon from "../../assets/images/icon-person.svg";
-import { AppWrapperStyled, InputsWrapper } from "./styles";
-import useTipValues from "../../hooks/useTipValues";
-import {
-  decimalValidation,
-  integerValidation,
-} from "../../utils/validations/formValidations";
-import useInputData from "../../hooks/useInputData";
+import { AppWrapperStyled } from "./styles";
+import InputsPanel from "../InputsPanel";
 
 const TipCalculator = () => {
-  const { setBill, setPeople } = useTipValues();
-  const { inputData, setInputData, inputErrors, setInputErrors } =
-    useInputData();
-
   return (
     <AppWrapperStyled>
-      <InputsWrapper>
-        <LabeledInput
-          id="price"
-          label="Bill"
-          iconSrc={dollarIcon}
-          setValue={setBill}
-          inputValue={inputData.bill}
-          setInputValue={value => setInputData({ ...inputData, bill: value })}
-          error={inputErrors.bill}
-          setError={error => setInputErrors({ ...inputErrors, bill: error })}
-          validations={decimalValidation}
-        />
-        <TipMenu label="Select Tip %" />
-        <LabeledInput
-          id="people"
-          label="Number of People"
-          iconSrc={personIcon}
-          setValue={setPeople}
-          inputValue={inputData.people}
-          setInputValue={value => setInputData({ ...inputData, people: value })}
-          error={inputErrors.people}
-          setError={error => setInputErrors({ ...inputErrors, people: error })}
-          validations={integerValidation}
-        />
-      </InputsWrapper>
+      <InputsPanel />
       <ResultPanel />
     </AppWrapperStyled>
   );
